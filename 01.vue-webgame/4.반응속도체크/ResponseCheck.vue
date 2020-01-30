1e0178da5e6a2ee531d45220a076d83e4ac796b0
tpdud3254 <tpdud3254@naver.com>
2020-01-30 18:58:51 +0900

    점점 어려웡

--- 01.vue-webgame/4.반응속도체크/ResponseCheck.vue
+++ 01.vue-webgame/4.반응속도체크/ResponseCheck.vue
@@ -1,22 +1,72 @@
 <template>
   <div>
-    <div id="screen"></div>
+    <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
+    <!--  v-bind: 축약형 : -->
+    <!-- <div id="screen" v-bind:class="state">{{ message }}</div> -->
     <div>
       <div>평균 시간 : {{}}</div>
-      <button @click="onReset"></button>
+      <button @click="onReset">리셋</button>
     </div>
   </div>
 </template>
-
+<!--
+FIXME: npm i vue-style-loader -D, npm i css-loader -D 후 webpack.config.js 수정
+-->
 <script>
+let startTime = 0;
+let endTime = 0;
+let avg = null;
+
 export default {
   data() {
-    return {};
+    return {
+      result: [],
+      state: "waiting",
+      message: "클릭해서 시작하세요."
+    };
   },
   methods: {
-    onReset() {}
+    onReset() {},
+    onClickScreen() {
+      if (this.state === "waiting") {
+        this.state = "ready";
+        this.message = "초록색이 되면 클릭하세요.";
+        setTimeout(() => {
+          (this.state = "now"), (this.message = "지금 클릭");
+        }, Math.floor(Math.random() * 1000) + 2000); //2-3초
+      } else if (this.state === "ready") {
+        this.state = "waiting";
+        this.message = "너무 성급하시군요! 초록색이 된 후에 클릭하세요.";
+      } else if (this.state === "now") {
+        this.state = "waiting";
+        this.message = "클릭해서 시작하세요.";
+      }
+    }
   }
 };
 </script>
 
-<style></style>
+<!--
+scoped : 이 컴포넌트 안에서만 쓰고싶을때, 해당 css들은 해당 컴포넌트에서만 쓸수있음
+ -->
+<style scoped>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+
+#screen.waiting {
+  background-color: aqua;
+}
+
+#screen.ready {
+  background-color: red;
+  color: white;
+}
+
+#screen.now {
+  background-color: greenyellow;
+}
+</style>
--- 01.vue-webgame/4.반응속도체크/webpack.config.js
+++ 01.vue-webgame/4.반응속도체크/webpack.config.js
@@ -1,24 +1,36 @@
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const path = require("path");
 
+/*
+module (loader) : 여러가지의 파일들을 하나의 자바스크립트파일로 만들어줌. .vue .css전부 (웹팩의 핵심)
+ plugins : 그외에 추가적으로 아웃풋전에 해야할것들 해준다고 생각하면됨
+*/
+
 module.exports = {
-    mode: 'development',
-    devtool: 'eval',
-    resolve: {
-        extensions: ['.js', '.vue'],
-    },
-    entry: {
-        app: path.join(__dirname, "./main.js")
-    },
-    module: {
-        rules: [{
-            test: /\.vue$/,
-            loader: "vue-loader"
-        }]
-    },
-    plugins: [new VueLoaderPlugin()],
-    output: {
-        filename: "[name].js",
-        path: path.join(__dirname, "./dist")
-    }
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".vue"]
+  },
+  entry: {
+    app: path.join(__dirname, "./main.js")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "./dist"),
+    publicPath: "/dist"
+  }
 };
